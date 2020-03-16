3061d5d860de87c1d207f1d539c53268d61f12a9
YTJ <593801655@qq.com>
2020-03-16 14:34:40 +0800

    report eacherts

---
+++ src/components/report/Report.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <div id="main" style="width: 750px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+<script>
+import echarts from "echarts";
+export default {
+  name: "",
+  data() {
+    return {
+      options: {
+        title: {
+          text: "用户来源"
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "#E9EEF3"
+            }
+          }
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: "value"
+          }
+        ]
+      }
+    };
+  },
+  created() {},
+  //dom渲染完毕
+  async mounted() {
+    var myChart = echarts.init(document.getElementById("main"));
+    const { data: res } = await this.$http.get("reports/type/1");
+    if (res.meta.status !== 200) {
+      return this.$message.error("获取图表数据失败");
+    }
+     
+    // 使用刚指定的配置项和数据显示图表。
+    Object.assign(this.options, res.data);
+    console.log(res.data)
+    myChart.setOption(this.options);
+  },
+  methods: {}
+};
+</script>
+<style scoped lang='less'>
+</style>
